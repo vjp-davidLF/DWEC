f138902ea16b46980280f285b6f1f5fa4b6c2d3b
vjp-davidLF <dlopezf03@informatica.iesvalledeljerteplasencia.es>
2025-10-03 18:30:59 +0200

    subir

---
+++ UT3/UT3-Prueba3/UT3-Prueba6/prueba6.1.js
@@ -0,0 +1,8 @@
+let body = document.body;
+console.log(body.childNodes);
+console.log(body.children);
+console.log(body.parentNode);
+console.log(body.nextSibling);
+console.log(body.previousSibling);
+console.log(body.nextElementSibling);
+console.log(body.previousElementSibling);
---
+++ UT3/UT3-Prueba3/UT3-Prueba7/prueba7.js
@@ -0,0 +1,13 @@
+for(let i = 0; i < elementosP.length; i++) {
+    console.log(elementosP[i]); 
+}
+
+while(document.getElementsByClassName("parrafo").length > 0){
+    document.getElementsByClassName("parrafo")[0].remove();
+}
+
+for(let i = 0; i < elementosP.length; i++) {
+    console.log(elementosP[i]); 
+}
+
+
