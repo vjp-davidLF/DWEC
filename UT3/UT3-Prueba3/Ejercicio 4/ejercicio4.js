4a6c44c764e7609d6b1b87cdefd2b9b5785ec876
vjp-davidLF <dlopezf03@informatica.iesvalledeljerteplasencia.es>
2025-10-09 18:28:42 +0200

    asdf

---
+++ UT3/UT3-Prueba3/Ejercicio 4/ejercicio4.js
@@ -0,0 +1,6 @@
+let div = document.getElementById("normalDiv");
+div.style.boxSizing;
+div.style.maxWidth = "200px";
+div.style.padding = "50px";
+div.style.color = "white";
+div.style.backgroundColor = "red";
---
+++ UT3/UT3-Prueba3/Ejercicio 5/ejercicio5.js
@@ -0,0 +1,14 @@
+let ventana = ventana.addEventListener("DOMContentLoaded", function () {
+    let estilos = document.getElementById("ContenedorGeneral");
+    div.style.width = "200px";
+    div.style.height = "200px";
+    div.style.backgroundColor = "#9e9e9e";
+    div.style.color = "blue";
+    div.style.border = "1px,red";
+
+    window.addEventListener("DOMContentLoaded", function(){
+        div.innerText = window.innerWidth + "-" + window.innerHeight;
+    })
+})
+
+
